556a2fa448348fceeaace474e4f7938d58ec91a7
X67 Deploy <deploy@x67digital.com>
2026-03-18 13:03:21 +0000

    auto-commit for 0cbbc6a6-0c48-4014-aca0-dfe2f412e922

--- frontend/src/pages/CategoryPage.jsx
+++ frontend/src/pages/CategoryPage.jsx
@@ -5,6 +5,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import AdCard from "../components/AdCard";
 import SEOHead from "../components/SEOHead";
+import AgeVerificationModal from "../components/AgeVerificationModal";
 import { Button } from "../components/ui/button";
 import {
   Select,
